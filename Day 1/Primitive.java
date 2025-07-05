d98e2185d4fb5ebf9c0883eb4c5a51bc1bb22246
Praveen Tiwari <Praveentiwari070615@gmail.com>
2025-07-06 01:44:47 +0530

    Add files via upload

---
+++ Day 1/Primitive.java
@@ -0,0 +1,17 @@
+
+
+public class Primitive {
+    public static void main(String[] args){
+        int rno = 60;
+        float b = 98.67f;
+        char c = 'P';
+        double d = 87.38776627;
+        long e = 25_00_000;
+
+        System.out.println("My Roll No. is : " + rno);
+        System.out.println("Float value: " + b);
+        System.out.println("Name: " + c);
+        System.out.println("Double value: " + d);
+        System.out.println("Long value: " + e);
+    }
+}
---
+++ Day 1/sum.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class sum {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the Two numbers : ");
+        int num1 = input.nextInt();
+        int num2 = input.nextInt();
+        int sum = num1+num2;
+        System.out.print("\nSum of two numbers is : " + sum);
+    }
+}
