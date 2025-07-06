9e4f3fe109ecbc73169afcae74b1ccafc92ff80f
Praveen Tiwari <Praveentiwari070615@gmail.com>
2025-07-06 17:29:21 +0530

    Add files via upload

---
+++ Day_2/if_and_loop.java
@@ -0,0 +1,30 @@
+package Day_2;
+public class if_and_loop {
+    public static void main(String[] args){
+        int a=10;
+        if(a>0)
+        {
+            System.out.println("Number is Positive");
+        }
+        else if(a==0)
+        {
+            System.out.println("Number is Zero!");
+        }
+        else
+        {
+            System.out.println("Number is Negative");
+        }
+
+        int i =1;
+        while(i<=5)
+        {
+            System.out.println(i);
+            i++;
+        }
+
+        for(int j=6;j<=10;j++)
+        {
+            System.out.println(j);
+        }
+    }
+}
