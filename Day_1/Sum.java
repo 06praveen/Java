b476dc6757324deb2db2c2c865e78ffdcdffd0ee
Praveen Tiwari <Praveentiwari070615@gmail.com>
2025-07-06 18:22:12 +0530

    Update and rename sum.java to Sum.java

--- Day_1/Sum.java
+++ Day_1/Sum.java
@@ -1,6 +1,6 @@
 package Day_1;
 import java.util.Scanner;
-public class sum {
+public class Sum {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter the Two numbers : ");
