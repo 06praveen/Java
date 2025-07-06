739133526e7d9909f3c9ab4b233ac4689e7e137c
Praveen Tiwari <Praveentiwari070615@gmail.com>
2025-07-06 18:21:52 +0530

    Update and rename tempconvert.java to Tempconvert.java

--- Day_2/Tempconvert.java
+++ Day_2/Tempconvert.java
@@ -1,7 +1,7 @@
 package Day_2;
 import java.util.Scanner;
 
-public class tempconvert {
+public class Tempconvert {
     public static void main(String[] args) {
      Scanner input = new Scanner(System.in);
      System.out.print("Enter the temp in °C : ");
