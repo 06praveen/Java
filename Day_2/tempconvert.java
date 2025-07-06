bd17e40c1e1cd6ba1902cc14935bf6af4e4d43ed
Praveen Tiwari <Praveentiwari070615@gmail.com>
2025-07-06 17:50:36 +0530

    Add files via upload

---
+++ Day_2/tempconvert.java
@@ -0,0 +1,12 @@
+package Day_2;
+import java.util.Scanner;
+
+public class tempconvert {
+    public static void main(String[] args) {
+     Scanner input = new Scanner(System.in);
+     System.out.print("Enter the temp in °C : ");
+     float tempC = input.nextFloat();
+     float tempF = (tempC * (9/5)) + 32;
+     System.out.println("The Temp " + tempC + "°C = " + tempF + "°F");
+    }
+}
