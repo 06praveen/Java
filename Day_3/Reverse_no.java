0ddd712692eecb620bb77e1c904e791b7e21c83f
Praveen Tiwari <Praveentiwari070615@gmail.com>
2025-07-08 00:40:24 +0530

    Add files via upload

---
+++ Day_3/Reverse_no.java
@@ -0,0 +1,18 @@
+package Day_3;
+import java.util.Scanner;
+
+public class Reverse_no {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the number to reverse : ");
+        long n = in.nextLong();
+        long i=0;
+        while(n>0)
+        {
+            long rem = n%10;
+            i=i*10+rem;
+            n=n/10;
+        }
+         System.out.print("The reverse number is : "+i);
+    }
+}
