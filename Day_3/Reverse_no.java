57c69f792f11ab8673377cdb8ecdba17490a8c85
Praveen Tiwari <Praveentiwari070615@gmail.com>
2025-07-08 00:41:46 +0530

    Update Reverse_no.java
    Added comments

--- Day_3/Reverse_no.java
+++ Day_3/Reverse_no.java
@@ -5,12 +5,12 @@ public class Reverse_no {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Enter the number to reverse : ");
-        long n = in.nextLong();
+        long n = in.nextLong();      //long is use for big numbers
         long i=0;
         while(n>0)
         {
             long rem = n%10;
-            i=i*10+rem;
+            i=i*10+rem;         //the number get added is reverse order
             n=n/10;
         }
          System.out.print("The reverse number is : "+i);
