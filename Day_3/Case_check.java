53077ba5b2b016112f30e3ecdb589ec25e71de03
Praveen Tiwari <Praveentiwari070615@gmail.com>
2025-07-07 23:32:11 +0530

    Update Case_check.java

--- Day_3/Case_check.java
+++ Day_3/Case_check.java
@@ -1,1 +1,22 @@
+package Day_3;
+import java.util.Scanner;
 
+public class Case_check{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the character to check : ");
+        char ch = in.next().trim().charAt(0);         //trim is use to avoid spacing
+        if(ch>='a' && ch<='z')
+        {
+            System.out.println("Character : "+ch+" is in Lowercase");
+        }
+        else if(ch>='A' && ch<='Z')
+        {
+            System.out.println("Character : "+ch+" is in Uppercase");
+        }
+        else
+        {
+            System.out.println("Invalid Character!");
+        }
+    }
+}
