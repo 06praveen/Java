686d7773f9d485beb204c7f087ed5f83dc8516e1
Praveen Tiwari <Praveentiwari070615@gmail.com>
2025-07-08 00:10:26 +0530

    Update Count_occurence.java

--- Day_3/Count_occurence.java
+++ Day_3/Count_occurence.java
@@ -5,18 +5,18 @@ public class Count_occurence {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Enter the number : ");
-        long n = in.nextLong();
+        long n = in.nextLong();   //long is used because the digit may big ex:phone no.
         System.out.print("Enter the number to check occurence : ");
         int p = in.nextInt();
         int i = 1;
         int count = 0;
         while (n>0)
         {
-            Long rem = n % 10;
+            Long rem = n % 10;   //By using this we will get the last digit of no.
             if(rem==p){
                 count += 1;
             }
-            n=n/10;
+            n=n/10;   //The last digit gets removed
         }
         System.out.print("The Occurence of "+p+" is "+count);
     }
