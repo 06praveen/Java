0fab598cee6b6f5d2cf1caf9090a944188c00b63
Praveen Tiwari <Praveentiwari070615@gmail.com>
2025-07-09 23:20:28 +0530

    Update and rename Switch_loop to Switch_loop.java

---
+++ Day_4/Switch_loop.java
@@ -0,0 +1,21 @@
+//Used latest switch case syntax
+package Day_4;
+import java.util.Scanner;
+
+public class Day_find {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number to find the Day : ");
+        int day = input.nextInt();
+        switch(day){
+            case 1 -> System.out.print("The Day is : Monday");
+            case 2 -> System.out.print("The Day is : Tuesday");
+            case 3 -> System.out.print("The Day is : Wednesday");
+            case 4 -> System.out.print("The Day is : Thursday");
+            case 5 -> System.out.print("The Day is : Friday");
+            case 6 -> System.out.print("The Day is : Saturday");
+            case 7 -> System.out.print("The Day is : Sunday");
+            default -> System.out.print("Invalid Input!");
+        }
+    }
+}
