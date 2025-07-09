6e6334da779346eee2fc998e1dec8ebe0e47cf22
Praveen Tiwari <Praveentiwari070615@gmail.com>
2025-07-09 22:12:23 +0530

    Update and rename Switch.java to Switch_basic.java

---
+++ Day_4/Switch_basic.java
@@ -0,0 +1,31 @@
+package Day_4;
+import java.util.Scanner;
+
+public class Switch_basic{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the name of Fruit : ");
+        String fruits = input.next().trim().toLowerCase();
+        
+        switch(fruits)
+        {
+            case "mango":
+            System.out.println("You are Sweet as Mango😙");
+            break;
+            case "banana":
+            System.out.println("You have stregth like Banana🫡");
+            break;
+            case "apple":
+            System.out.println("You are Healthy as a Apple🦾");
+            break;
+            case "orange":
+            System.out.println("You have a face like orange🥲");
+            break;
+            case "grapes":
+            System.out.println("You are long Friend circle🤝");
+            break;
+            default:
+            System.out.println("Please Enter common Fruits🤖!");
+        }
+    }
+}
