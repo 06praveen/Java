6cb25412ac820c1c37517075674f93c09ef879c8
Praveen Tiwari <Praveentiwari070615@gmail.com>
2025-07-11 23:16:04 +0530

    Update Shadowing.java

--- Day_6/Shadowing.java
+++ Day_6/Shadowing.java
@@ -1,6 +1,17 @@
-package Day_6
+package Day_6;
 
 public class Shadowing{
-  int x = 18;
+static int x = 18;     //Scope have in whole program 
   public static void main(String[] args){
-    
+    System.out.println("The value of x(Scope:Whole Program) = "+x);   //x=18
+    int x = 7;     //Scope only main function (Shadowed the x here)
+    System.out.println("The value of x(Scope:main) = "+x);       //x=7
+    System.out.println("The value of x(Scope:scope function) = "+scope());    //x=45
+
+  }
+
+  static int scope(){
+    int x = 45;       //Scope only in scope function
+    return x;
+  }
+}
