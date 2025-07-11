dfb0c43c953bc364c5e5b59f45bd628c3f31aa90
Praveen Tiwari <Praveentiwari070615@gmail.com>
2025-07-12 01:19:41 +0530

    Add files via upload

---
+++ Day_6/Prime_no.java
@@ -0,0 +1,33 @@
+package Day_6;
+import java.util.Scanner;
+
+public class Prime_no {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the number to check : ");
+        int num = in.nextInt();
+        boolean IsPrime=true;
+        if(num<=1)
+        {
+            System.out.println("The number "+num+" not a Prime Number");
+        }
+        else{
+        for(int i=2;i<num;i++)
+        {
+            if(num%i==0)
+            {
+                IsPrime = false;
+            }
+        }
+        if(IsPrime==false)
+        {
+            System.out.println("The number "+num+" not a Prime Number");
+        }
+        else
+        {
+            System.out.println("The number "+num+" a Prime Number");
+
+        }
+    }
+    }    
+}
