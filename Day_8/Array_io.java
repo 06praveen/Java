ae1b666e0739301050ee429ae0790f878e3de6ef
Praveen Tiwari <Praveentiwari070615@gmail.com>
2025-07-14 00:10:48 +0530

    Added comments

--- Day_8/Array_io.java
+++ Day_8/Array_io.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class Array_io {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int[] arr = new int[5];
+        int[] arr = new int[5];                  //Initializing an array
         System.out.print("Enter the numbers : ");
-        for(int i =0;i<5;i++)
+        for(int i =0;i<5;i++)                    //Taking array input
         {
             arr[i] = in.nextInt();
         }
-        for(int i =0;i<5;i++)
+        for(int i =0;i<5;i++)                    //Displaying elements of array
         {
             System.out.print(arr[i]+ " ");
         }
