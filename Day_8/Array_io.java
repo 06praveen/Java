de635150683ed78680b5b6497c21ce18161feb58
Praveen Tiwari <Praveentiwari070615@gmail.com>
2025-07-14 00:09:08 +0530

    Update Array_io.java

--- Day_8/Array_io.java
+++ Day_8/Array_io.java
@@ -1,1 +1,19 @@
+//Starting Array
+package Day_8;
+import java.util.Scanner;
 
+public class Array_io {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] arr = new int[5];
+        System.out.print("Enter the numbers : ");
+        for(int i =0;i<5;i++)
+        {
+            arr[i] = in.nextInt();
+        }
+        for(int i =0;i<5;i++)
+        {
+            System.out.print(arr[i]+ " ");
+        }
+    }
+}
