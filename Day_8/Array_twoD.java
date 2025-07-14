1e799e0491f3a223073430b6698f041d993dcfba
Praveen Tiwari <Praveentiwari070615@gmail.com>
2025-07-15 00:36:45 +0530

    Update Array_twoD.java

--- Day_8/Array_twoD.java
+++ Day_8/Array_twoD.java
@@ -1,1 +1,27 @@
-// Learning 2D Array
+//2D array used to create matrix
+package Day_8;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Array_twoD {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int[][] arr = new int[3][3];
+        System.out.print("Enter the Arrays[2D] Elements : ");
+        for(int i=0;i<arr.length;i++)
+        {
+            for(int j=0;j<3;j++)
+            {
+                arr[i][j] = in.nextInt();
+            }
+        }
+        System.out.print(Arrays.deepToString(arr)+"\n");
+
+        int[][] arr2D = {
+            {1,2,3},
+            {4,5,6},
+            {7,8,9}
+        };
+        System.out.print(Arrays.deepToString(arr2D)+"\n");
+    }
+}
