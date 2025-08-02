54da5f601da9342677dbaeed40f0b1a31ccb7b9d
Praveen Tiwari <Praveentiwari070615@gmail.com>
2025-08-02 16:08:12 +0530

    Update Fibonacci.java

--- Day_3/Fibonacci.java
+++ Day_3/Fibonacci.java
@@ -4,18 +4,19 @@ import java.util.Scanner;
 public class Fibonacci{
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter the number till you want : ");
+        System.out.print("Enter the nth term number : ");
         int n = in.nextInt();
-        int a=0;
-        int b=1;
-        System.out.print("Fibonacci Series is : "+a+" ");
+        int a=0,b=1;
+        int temp;
+        System.out.print("Fibonacci Series is : "+a+" "+b+" ");
         for(int i=2;i<=n;i++)
         {
-            int temp=b;
+            temp=b;
             b = b+a;
             a = temp;
             System.out.print(b+" ");
         }
 
     }
+
 }
