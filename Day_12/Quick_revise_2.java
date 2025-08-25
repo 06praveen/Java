5056804855c3a5a2325f68cddf8867070c6625e8
Praveen Tiwari <Praveentiwari070615@gmail.com>
2025-08-25 22:21:44 +0530

    Revised the functions using parameter (factorial)

---
+++ Day_12/Quick_revise_2.java
@@ -0,0 +1,20 @@
+package Day_12;
+import java.util.Scanner;
+
+public class Quick_revise_2 {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the no. to find factorial : ");
+        int n = in.nextInt();
+        System.out.print("The Factorial of "+n+" is : "+factorial(n));
+    }
+
+    static int factorial(int x){
+        int fact=1;
+        for(int i=2;i<=x;i++)
+        {
+            fact=fact*i;
+        }
+        return fact;
+    }
+}
