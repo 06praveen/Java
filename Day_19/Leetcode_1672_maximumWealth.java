84d3c8a6c083cd28ce0b468e927d9164cdf699c9
Praveen Tiwari <Praveentiwari070615@gmail.com>
2025-09-18 19:00:07 +0530

    Solved the leetcode problem to find the maximum wealth [2D Array]

--- Day_19/Leetcode_1672_maximumWealth.java
+++ Day_19/Leetcode_1672_maximumWealth.java
@@ -1,18 +1,19 @@
+//Find the maximum wealth of customer [2-D Array]
 class Solution {
     public int maximumWealth(int[][] accounts) {
         int max=0;
-        for(int i=0;i<accounts.length;i++)
+        for(int i=0;i<accounts.length;i++)     //Customers
         {
             int sum=0;
-            for(int j=0;j<accounts[i].length;j++)
+            for(int j=0;j<accounts[i].length;j++)     //Bank Accounts
             {
-                sum=sum+accounts[i][j];
+                sum=sum+accounts[i][j];          //Total wealth of each customer
             }
             if(sum>max)
             {
                 max=sum;
             }
         }
-        return max;
+        return max;           //Maximum wealth among all the customer
     };
 }
