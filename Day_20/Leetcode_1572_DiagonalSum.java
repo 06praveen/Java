99b54a91b89d8f5021767a3885908161ad59f95e
Praveen Tiwari <Praveentiwari070615@gmail.com>
2025-09-19 23:57:31 +0530

    Solved the leetcode question to sum the diagonal of matrix
    Solved the leetcode question to return the sum of diagonal elements of a 2D Array

--- Day_20/Leetcode_1572_DiagonalSum.java
+++ Day_20/Leetcode_1572_DiagonalSum.java
@@ -1,3 +1,4 @@
+//Solved the leetcode question to return the sum of diagonal elements of a 2D Array
 class Solution {
     public int diagonalSum(int[][] mat) {
         int sum=0;
@@ -7,14 +8,14 @@ class Solution {
                     {
                         if(i==j)
                         {
-                            sum = sum + mat[i][j];
+                            sum = sum + mat[i][j];     //left to right Diagonal Elemnts are added
                         }
-                        if(i+j==mat.length-1)
+                        if(i+j==mat.length-1)        
                         {
                             if(i==j)
                             {continue;}
                             else
-                            {sum = sum + mat[i][j];}
+                            {sum = sum + mat[i][j];}        //right to left Diagonal elements are added
                         }
                     }
             }
