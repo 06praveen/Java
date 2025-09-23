727ad23bffb3bbf4e5c46151ffcd67d4420c02a7
Praveen Tiwari <Praveentiwari070615@gmail.com>
2025-09-23 22:47:06 +0530

    Solved the leetcode problem to Running Sun of 1D Array

---
+++ Day_24/Leetcode_1480_RunningSum1.java
@@ -0,0 +1,15 @@
+//Solved the leetcode problem to Running Sun of 1D Array
+class Solution {
+    public int[] runningSum(int[] nums) {
+        int[] arr = new int[nums.length];
+        for(int i=0;i<nums.length;i++)
+        {
+            for(int j=0;j<i+1;j++)
+            {
+              arr[i] = arr[i]+nums[j];
+            }
+        }
+        return arr;
+    };
+}
+//Time Complexity - O(N²)
