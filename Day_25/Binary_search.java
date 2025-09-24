0e93323b97f4961400d369f4aae32ca0a4fe3dd5
Praveen Tiwari <Praveentiwari070615@gmail.com>
2025-09-24 21:05:53 +0530

    Practiced the code for Binary Search

---
+++ Day_25/Binary_search.java
@@ -0,0 +1,32 @@
+package Day_5;
+
+public class Binary_search {
+    public static void main(String[] args){
+        int[] arr = {2,5,7,9,14,18,24,27};
+        int target = 18;
+        search(arr,target);
+    }    
+    public static void search(int[] arr,int x){
+        int s=0,e=arr.length-1;
+        boolean z = false;
+        while(s<e)
+        {
+            int mid = (s+e)/2;
+            if(arr[mid]==x)
+            {
+                z = true;
+                break;
+            }
+            else if(arr[mid]<x)
+            {
+                s = mid+1;
+            }
+            else
+            {
+                e = mid-1;
+            }
+        }
+        System.out.print("Target found is "+z);
+    }
+    
+}
