7f4965e041ded9aad937561ccdbda069a2fa0c1b
Praveen Tiwari <Praveentiwari070615@gmail.com>
2025-09-28 18:57:09 +0530

    Create Leetcode_167_twoSumII.java

---
+++ Day_29/Leetcode_167_twoSumII.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int s=0,e=numbers.length-1;
+        int sum=0;
+        while(s<=e)
+        {
+            sum=numbers[s]+numbers[e];
+            if(sum==target)
+            {return new int[] {s+1,e+1};}
+            else if(sum>target)
+            {e--;}
+            else
+            {s++;}
+        }
+        return new int[] {0};
+    }
+}
