1ac4b6005e1246a37ce19d348d24f24759a9a93e
Praveen Tiwari <Praveentiwari070615@gmail.com>
2025-09-28 19:23:11 +0530

    Solved the leetcode problem to find target with less time & space complexity
    Solved the leetcode problem to find target with less time and space complexity. This is the 2nd Solution with better time complexity

--- Day_29/Leetcode_167_twoSumII.java
+++ Day_29/Leetcode_167_twoSumII.java
@@ -1,8 +1,9 @@
+//Solved the leetcode problem to find target with less time and space complexity
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
         int s=0,e=numbers.length-1;
         int sum=0;
-        while(s<=e)
+        while(s<=e)                     //Using Binary Search algorithm
         {
             sum=numbers[s]+numbers[e];
             if(sum==target)
@@ -15,3 +16,5 @@ class Solution {
         return new int[] {0};
     }
 }
+// Time Complexity - O(n)
+// Space Complexity - O(1)
