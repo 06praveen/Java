1bed7492501d8759b504b3798497607925ccc339
Praveen Tiwari <Praveentiwari070615@gmail.com>
2025-11-09 19:21:16 +0530

    Ceiling of Number using Binary Search in Java

--- Binary_Search/CeilingOfNumber.java
+++ Binary_Search/CeilingOfNumber.java
@@ -1,7 +1,9 @@
+//Code in java to search a number in array or the smallest number among the gretaest number than the key value
+package Binary_Search;
 import java.util.Scanner;
 
 public class CeilingOfNumbers{
-    public static void main(String[] args){
+    public static void main(String[] args){     //main function
         Scanner in = new Scanner(System.in);
         System.out.print("Enter the size of array : ");
         int n = in.nextInt();
@@ -9,11 +11,11 @@ public class CeilingOfNumbers{
         System.out.print("Enter te elements of array : ");
         for(int i=0;i<n;i++)
         {
-            arr[i] = in.nextInt();
+            arr[i] = in.nextInt();                        //Array input from user
         }
         System.out.print("Enter the number to search : ");
         int key = in.nextInt();
-        int result = CeilingSearch(arr,key);
+        int result = CeilingSearch(arr,key);             //Calling the method by value and storing the result;
         if(result==key)
         {
             System.out.print("The elements "+key+"is found");
@@ -25,9 +27,9 @@ public class CeilingOfNumbers{
         in.close();
     }
 
-    public static int CeilingSearch(int[] arr,int target){
+    public static int CeilingSearch(int[] arr,int target){          //Ceiling Logic method
         int s=0,e=arr.length-1;
-        while(s<=e)
+        while(s<=e)                                       //Binary Search Logic
         {
             int mid = s+((e-s)/2);
             if(arr[mid]==target)
@@ -43,6 +45,6 @@ public class CeilingOfNumbers{
                 e=mid-1;
             }
         }
-        return arr[s];
+        return arr[s];                    //Returns the smallest among the greatest numbers than the key
     }
 }
