88e885d0c7e8bc9675b228f84dadd329e06d8f15
Praveen Tiwari <Praveentiwari070615@gmail.com>
2025-12-27 19:18:23 +0530

    Solved the leetcode problem to find the target in a rotated sorted array
    Implemented  to find the target in a rotated sorted array using binary search techniques.

---
+++ Leetcode/33_rotatedSortedArray.java
@@ -0,0 +1,49 @@
+//We have search the target in the given rotated sorted array.
+//It is same as the mountain array but a little change in logic to find the pivot i.e the peak element of the array
+class Solution {
+    public int search(int[] arr, int target) {
+        int n=arr.length;
+        int pivot = pivotSearch(arr);
+        int result = AgnosticBS(arr,target,0,pivot);     //Searching on the left handside of the pivot
+        if(result==-1)
+        {
+            result=AgnosticBS(arr,target,pivot+1,n-1);    //Searching on the right handside of the peak if target is not found on the left
+        }
+        return result;
+    }
+
+  //To find the pivot i.e the peak element in the given array
+    public int pivotSearch(int[] arr){
+        int n=arr.length;
+        int s=0,e=n-1;
+        while(e>s)
+        {
+            int mid=s+(e-s)/2;
+            if(arr[mid]>arr[mid+1])
+            {
+                return mid;
+            }
+            else if(arr[mid]>=arr[s])
+            {s=mid+1;}
+            else
+            {e=mid;}
+        }
+        return 0;
+    }
+
+  //Normal Binary Search Method to find the target
+    public int AgnosticBS(int[] arr,int target,int s,int e){
+        int ans=-1;
+        while(e>=s)
+        {
+            int mid=s+(e-s)/2;
+            if(arr[mid]==target)
+            {return mid;}
+            else if(arr[mid]>target)
+            {e=mid-1;}
+            else
+            {s=mid+1;}
+        }
+        return ans;
+    }
+}
