463eca5ed8026294e5719f435d9d2a13f8972da9
Praveen Tiwari <Praveentiwari070615@gmail.com>
2026-01-02 22:08:11 +0530

    Solved the leetcode problem to find repeated element in array
    Implement a method to find the repeated element in an array.

---
+++ Leetcode/961_nRepeatedElement.java
@@ -0,0 +1,16 @@
+//We have to find the repeated element in the given array and return the repeated integer
+//array contain 'n+1' unique element
+class Solution {
+    public int repeatedNTimes(int[] arr) {
+        int n=arr.length;
+        for(int i=0;i<n-1;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                if(arr[i]==arr[j])
+                {return arr[i];}
+            }
+        }
+        return -1;
+    }
+}
