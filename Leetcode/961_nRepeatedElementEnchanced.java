62d1f4aa8897b251a2ac2bd972fc58b3a6a30231
Praveen Tiwari <Praveentiwari070615@gmail.com>
2026-01-03 14:09:36 +0530

    Add O(n) solution for finding repeated element
    Implemented a solution to find the repeated element in O(n) time complexity.

---
+++ Leetcode/961_nRepeatedElementEnchanced.java
@@ -0,0 +1,16 @@
+//Solved this in O(n) time complexity which is better than my past approach
+class Solution {
+    public int repeatedNTimes(int[] arr) {
+        int n=arr.length;
+        for(int i=0;i<n-1;i++)
+        {
+            if(arr[i]==arr[i+1])
+            {return arr[i];}
+            else if(i+2<n && arr[i]==arr[i+2])
+            {return arr[i];}
+            else if(i+3<n && arr[i]==arr[i+3])
+            {return arr[i];}
+        }
+        return 0;
+    }
+}
