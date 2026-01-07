3dde13b3ddea4a8445985237cef185ecb574122a
Praveen Tiwari <Praveentiwari070615@gmail.com>
2026-01-07 23:58:00 +0530

    Create 154_minimumRotatedSorted2.java

---
+++ Leetcode/154_minimumRotatedSorted2.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int findMin(int[] arr) {
+        int n=arr.length;
+        return binarySearch(arr,n);
+    }
+
+    public int binarySearch(int[] arr,int n){
+        int s=0,e=n-1;
+        while(e>s)
+        {
+            int mid=s+(e-s)/2;
+            if(arr[mid]>arr[e])
+            {
+                s=mid+1;
+            }
+            else if (arr[mid] < arr[e])
+            {
+                e = mid;
+            } 
+            else 
+            {
+                e--;
+            }
+        }
+        return arr[s];
+    }
+}
